8b6e6f3a674764b78df115f18067facbf173c3ac
David Iturriaga Sotelo <diturriagas@MacBook-Pro-de-David.local>
2023-10-26 14:06:41 -0700

    Add scripts

---
+++ scripts/testing.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KEYARRAY=(`kubectl get pods --namespace latam-challenge -o jsonpath="{.items[*].spec.containers[*].image}" -l app=latam-challenge-api`)
+OLDIMAGENAME=${KEYARRAY[1]}
+echo $OLDIMAGENAME
+
+envsubst < $k8sManifest | kubectl apply -f -
+
+python testapp/manage.py
+
+if [ $? -eq 0 ]; then
+    echo Succesfull Deployment
+else
+    echo RollBack Image
+    export imageName=$OLDIMAGENAME
+    envsubst < deployment.yml | kubectl apply -f -
+    echo RollBack Image Succesfull
+    raise error "Test cases failed !!!"
+fi
